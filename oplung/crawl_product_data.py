cc03531baf19e03f85bc9b2da7a287a14e9a1065
truongminhphongCV <truongminhphonguit@gmail.com>
2023-05-17 15:45:12 +0700

    add project

---
+++ oplung/crawl_product_data.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import requests
+import time
+import random
+from tqdm import tqdm
+
+cookies = {
+    'TIKI_GUEST_TOKEN': '8jWSuIDBb2NGVzr6hsUZXpkP1FRin7lY',
+    'TOKENS': '{%22access_token%22:%228jWSuIDBb2NGVzr6hsUZXpkP1FRin7lY%22%2C%22expires_in%22:157680000%2C%22expires_at%22:1763654224277%2C%22guest_token%22:%228jWSuIDBb2NGVzr6hsUZXpkP1FRin7lY%22}',
+    'amp_99d374': 'eSc-_0HT1um7cb57E7dwA0...1enloc6a2.1enlpc1q6.b.i.t',
+    'amp_99d374_tiki.vn': 'eSc-_0HT1um7cb57E7dwA0...1enloc6a2.1enlp8817.3.1.1',
+    '_gcl_au': '1.1.559117409.1605974236',
+    '_ants_utm_v2': '',
+    '_pk_id.638735871.2fc5': 'b92ae025fbbdb31f.1605974236.1.1605975278.1605974236.',
+    '_pk_ses.638735871.2fc5': '*',
+    '_trackity': '70e316b0-96f2-dbe1-a2ed-43ff60419991',
+    '_ga_NKX31X43RV': 'GS1.1.1605974235.1.1.1605975326.0',
+    '_ga': 'GA1.1.657946765.1605974236',
+    'ai_client_id': '11935756853.1605974227',
+    'an_session': 'zizkzrzjzlzizqzlzqzjzdzizizqzgzmzkzmzlzrzmzgzdzizlzjzmzqzkznzhzhzkzdzizhzdzizlzjzmzqzkznzhzhzkzdzizlzjzmzqzkznzhzhzkzdzjzdzhzqzdzizd2f27zdzjzdzlzmzmznzq',
+    'au_aid': '11935756853',
+    'dgs': '1605975268%3A3%3A0',
+    'au_gt': '1605974227146',
+    '_ants_services': '%5B%22cuid%22%5D',
+    '__admUTMtime': '1605974236',
+    '__iid': '749',
+    '__su': '0',
+    '_bs': 'bb9a32f6-ab13-ce80-92d6-57fd3fd6e4c8',
+    '_gid': 'GA1.2.867846791.1605974237',
+    '_fbp': 'fb.1.1605974237134.1297408816',
+    '_hjid': 'f152cf33-7323-4410-b9ae-79f6622ebc48',
+    '_hjFirstSeen': '1',
+    '_hjIncludedInPageviewSample': '1',
+    '_hjAbsoluteSessionInProgress': '0',
+    '_hjIncludedInSessionSample': '1',
+    'tiki_client_id': '657946765.1605974236',
+    '__gads': 'ID=ae56424189ecccbe-227eb8e1d6c400a8:T=1605974229:RT=1605974229:S=ALNI_MZFWYf2BAjzCSiRNLC3bKI-W_7YHA',
+    'proxy_s_sv': '1605976041662',
+    'TKSESSID': '8bcd49b02e1e16aa1cdb795c54d7b460',
+    'TIKI_RECOMMENDATION': '21dd50e7f7c194df673ea3b717459249',
+    'cto_bundle': 'V9Dkml9NVXNkQmJ6aEVLcXNqbHdjcVZoQ0l5RXpOMlRybjdDT0JrUnNxVXc2bHd0N2J3Y2FCdmZVQXdYY1QlMkJYUmxXSHZ2UEFwd3IzRHhzRWJYMlQlMkJsQjhjQlA1JTJCcExyRzlUQk5CYUdMdjl2STNQanVsa3cycHd3SHElMkJabnI3dzNhREpZcFVyandyM1d1QWpJbWYxT1UyWDdHZyUzRCUzRA',
+    'TIKI_RECENTLYVIEWED': '58259141',
+    '_ants_event_his': '%7B%22action%22%3A%22view%22%2C%22time%22%3A1605974691247%7D',
+    '_gat': '1',
+}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
+    'x-guest-token': '4EVnN7qkjeYHFIRvbGDU50oxTcMlrgSP',
+    'Connection': 'keep-alive',
+    'TE': 'Trailers',
+}
+
+params = (
+    ('platform', 'web'),
+    ('spid', 195277349)
+    #('include', 'tag,images,gallery,promotions,badges,stock_item,variants,product_links,discount_tag,ranks,breadcrumbs,top_features,cta_desktop'),
+)
+
+def parser_product(json):
+    d = dict()
+    d['ID'] = json.get('id')
+    d['Type'] = json.get('type')
+    d['SKU'] = json.get('sku')
+    d['Name'] = json.get('name')
+    d['brand_name'] = json.get('brand').get('name')
+    d['short_description'] = json.get('short_description')
+    d['price'] = json.get('list_price')
+    d['discount_price'] = json.get('price')   
+    d['thumbnail_url'] = json.get('thumbnail_url') + ',' + json.get('thumbnail_url') + ',' + json.get('thumbnail_url') + ',' + json.get('thumbnail_url')
+    d['category'] = 'Op lung'
+    d['description'] = json.get('description')
+    return d
+
+
+df_id = pd.read_csv('product_id_oplung.csv')
+p_ids = df_id.id.to_list()
+print(p_ids)
+result = []
+for pid in tqdm(p_ids, total=len(p_ids)):
+    response = requests.get('https://tiki.vn/api/v2/products/{}'.format(pid), headers=headers, params=params, cookies=cookies)
+    if response.status_code == 200:
+        print('Crawl data {} success !!!'.format(pid))
+        result.append(parser_product(response.json()))
+    # time.sleep(random.randrange(3, 5))
+df_product = pd.DataFrame(result)
+df_product.to_csv('product_data_oplung.csv', index=False)
